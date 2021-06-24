a11803e6ed282638a2f1760aac685909659efbe3
ShunSuzukiCarUsed <suzuki@carused.jp>
2021-06-24 11:28:43 +0900

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+import './bootstrap'
+import Vue from 'vue'
+
+Vue.component('example-component',require('./components/Sample.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
