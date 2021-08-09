eb99ac165ff29ace1678695487e866d8545e9a71
ShunSuzukiCarUsed <suzuki@carused.jp>
2021-08-09 10:53:04 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/sample', 'SampleController@index');
+Route::get('/sample', 'App\Http\Controllers\SampleController@index');
 
 Route::get('/', function () {
     return view('welcome');
