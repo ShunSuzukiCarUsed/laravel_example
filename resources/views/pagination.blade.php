2059537ef63979b4135d6c5a7862f825c853b4f5
ShunSuzukiCarUsed <suzuki@carused.jp>
2021-08-14 10:08:09 +0900

    Update pagination.blade.php

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -7,17 +7,17 @@
                 </li>
             @endif
 
-            @if ($paginator->currentPage() - 5 > 1)
+            @if ($paginator->currentPage() - 4 > 1)
                 <li>
                     <a href="{{ $paginator->url(1) }}">1</a>
                 </li>
             @endif
 
-            @if ($paginator->currentPage() - 6 > 1)
+            @if ($paginator->currentPage() - 5 > 1)
                 <li class="disabled" aria-disabled="true"><span>...</span></li>
             @endif
 
-            @for ($page = max(1, $paginator->currentPage() - 5); $page <= min($paginator->currentPage() + 5, $paginator->lastPage()); $page++)
+            @for ($page = max(1, $paginator->currentPage() - 4); $page <= min($paginator->currentPage() + 4, $paginator->lastPage()); $page++)
                 @if ($page == $paginator->currentPage())
                     <li class="active" aria-current="page"><span>{{ $page }}</span></li>
                 @else
@@ -25,11 +25,11 @@
                 @endif
             @endfor
 
-            @if ($paginator->currentPage() + 6 < $paginator->lastPage())
+            @if ($paginator->currentPage() + 5 < $paginator->lastPage())
                 <li class="disabled" aria-disabled="true"><span>...</span></li>
             @endif
 
-            @if ($paginator->currentPage() + 5 < $paginator->lastPage())
+            @if ($paginator->currentPage() + 4 < $paginator->lastPage())
                 <li>
                     <a href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a>
                 </li>
